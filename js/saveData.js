cadff6fe2a94e0906bde3ea5f00c7631f32af8e3
Clément KOENIG <clement.koenig@unistra.fr>
2019-04-25 20:56:55 +0200

    Put user's infos in the form if he has already saved them before

--- js/saveData.js
+++ js/saveData.js
@@ -1,11 +1,18 @@
-document.getElementById('saveData').onclick = saveData 
-
-function saveData() {
-
-    let infos = { "name": "", "email": "", "tel": "", "address": "",
+let infos = { "name": "", "email": "", "tel": "", "address": "",
         "address2": "",  "address3": "", "city": "", "zip_code": "",
         "country": "", "cb_type": "", "cb_number": "",
         "cb_month": "", "cb_year": "", "cvv": "" }
+
+chrome.storage.sync.get(["infos"], function(item){
+    if(Object.keys(item).length > 0) {
+        infos = item.infos
+        loadData()
+    }
+})
+
+document.getElementById('saveData').onclick = saveData 
+
+function saveData() {
 
     infos.name = document.getElementsByName('name')[0].value
     infos.email = document.getElementsByName('email')[0].value
@@ -25,4 +32,23 @@ function saveData() {
     infos.cvv = document.getElementsByName('cvv')[0].value
 
     chrome.storage.sync.set({ "infos": infos })
+}
+
+function loadData() {
+    document.getElementsByName('name')[0].value = infos.name
+    document.getElementsByName('email')[0].value = infos.email
+    document.getElementsByName('phone')[0].value = infos.tel
+
+    document.getElementsByName('address')[0].value = infos.address
+    document.getElementsByName('address2')[0].value = infos.address2
+    document.getElementsByName('address3')[0].value = infos.address3
+    document.getElementsByName('city')[0].value = infos.city
+    document.getElementsByName('zip_code')[0].value = infos.zip_code
+    document.getElementsByName('country')[0].value = infos.country 
+    
+    document.getElementsByName('cb_type')[0].value = infos.cb_type
+    document.getElementsByName('cb_number')[0].value = infos.cb_number
+    document.getElementsByName('cb_month')[0].value = infos.cb_month
+    document.getElementsByName('cb_year')[0].value = infos.cb_year
+    document.getElementsByName('cvv')[0].value = infos.cvv
 }
